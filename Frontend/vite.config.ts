42c6fcb1b19cffa732583d8292b6da520cf0cc37
unknown <rahulrajamasss55@gmail.com>
2025-09-11 20:55:32 +0530

    node module deleted

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ important for static hosting like Vercel
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
